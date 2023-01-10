4fbea64b2ff23284f3fc9f9b49b8c9c1af5f028d
abbydumke <111912949+abbydumke@users.noreply.github.com>
2023-01-10 17:53:19 -0500

    added two tests that work

--- test/utilities/ArraySetTest.java
+++ test/utilities/ArraySetTest.java
@@ -1,5 +1,9 @@
 package utilities;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 class ArraySetTest
@@ -12,6 +16,22 @@ class ArraySetTest
 	@Test
 	void testAddE()
 	{
+		
+		ArrayList<Integer> aL = new ArrayList<Integer>();
+		//test (adding 1 element)
+		int a1= 2;
+		aL.add(a1);
+		assertEquals(1, aL.size());
+		//test (adding 5 elements)
+		int a2 = 10;
+		aL.add(a2);
+		int a3 = 1;
+		aL.add(a3);
+		int a4 = 3;
+		aL.add(a4);
+		int a5 = 8;
+		aL.add(a5);
+		assertEquals(5, aL.size());
 	}
 
 	@Test
@@ -27,6 +47,28 @@ class ArraySetTest
 	@Test
 	void testRemoveAll()
 	{
+		ArrayList<Integer> aL = new ArrayList<Integer>();
+		//test (add 2 elements, remove all)
+		int a1= 2;
+		int a2 = 10;
+		aL.add(a1);
+		aL.add(a2);
+		aL.remove(0);
+		aL.remove(0);
+		assertEquals(0,aL.size());
+		
+		//test (add four elements to list, clear)
+		int a3 = 1;
+		int a4 = 2;
+		int a5 = 3;
+		int a6 = 4;
+		aL.add(a3);
+		aL.add(a4);
+		aL.add(a5);
+		aL.add(a6);
+		aL.clear();
+		assertEquals(0,aL.size());
+		
 	}
 
 	@Test
